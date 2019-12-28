b05e23bd85d32ed8f91d6d34dcd602b25ee4e3c1
Chris Cheong <chrisxcheong@gmail.com>
2019-12-28 20:12:02 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
+<<<<<<< HEAD
       create_table :students do |t|
+=======
+      create_table students do |t|
+>>>>>>> 04033204b2db8004896218887d92c0a6e6edcda4
         t.string :name
       end
   end
